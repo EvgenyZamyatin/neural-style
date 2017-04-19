09154e95062a88945fa0cbac2a4b484d7b18a011
evgenyzamyatin <jeka_zamyatin@mail.ru>
2017-04-19 20:15:26 +0300

    fix

--- neural_style/fast_neural_style/fast_neural_style.py
+++ neural_style/fast_neural_style/fast_neural_style.py
@@ -57,7 +57,7 @@ if args.subcommand is None:
 X = theano.shared(np.array([[[[]]]], dtype=floatX))
 alpha = theano.shared(np.array([[]], dtype=np.int32))
 
-S_COEFS = theano.shared(np.array([1/10, 1/5, 1/3, 1, 1.5], dtype=floatX))
+S_COEFS = theano.shared(np.array([1/10, 1/5, 1/3, 1, 1.5, 2, 3], dtype=floatX))
 
 
 weights = None if args.subcommand == "train" else args.model
